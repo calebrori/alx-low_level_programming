0f2d45a1d08def4f9f6d5d1f407bf99a5691ce02
calebrori <calebrori@gmail.com>
2023-03-17 00:30:36 +0300

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#iclude<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	pritf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
