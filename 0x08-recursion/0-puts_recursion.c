90b005a0a3f772faf7bf90315f7b23fa08bd53c7
calebrori <calebrori@gmail.com>
2023-04-06 00:04:17 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,15 @@
  * Return: a string, followed by a new line
  *
  */
-void _puts_recursion(char *s)                                                                                                           
-{                                                                                                                                       
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                _putchar(*s);                                                                                                           
-                _puts_recursion(s + 1);                                                                                                 
-        }                                                                                                                               
+void _puts_recursion(char *s)                                                                       
+{                                                                                                            
+        if (*s)
+        { 
+                _putchar(*s); 
+                _puts_recursion(s + 1);
+        } 
                                                                                                                                         
-        else                                                                                                                            
-                _putchar('\n');                                                                                                         
+           else 
+                _putchar('\n');
 }
 
