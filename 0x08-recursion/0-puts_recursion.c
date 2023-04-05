87827987ae9286d90544454c0b630d69b412c153
calebrori <calebrori@gmail.com>
2023-04-06 00:07:40 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,10 @@ void _puts_recursion(char *s)
 {                                                                                                            
         if (*s)
         { 
-                _putchar(*s); 
-                _puts_recursion(s + 1);
+		 _putchar(*s); 
+		 _puts_recursion(s + 1);
         } 
-                                                                                                                                        
-           else 
-                _putchar('\n');
-}
 
+	else
+		_putchar('\n');
+	}
