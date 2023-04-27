89a765a635bc74004db2b74f62306463e816b920
calebrori <calebrori@gmail.com>
2023-04-28 00:45:05 +0300

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,12 @@
 */
 void free_list(list_t *head)
 {
-   if (head)
-   {
-       free_list(head->next);
-       if (head->str)
-       free(head->str);
-       free(head);
-   }
+	if (head)
+	{
+	free_list(head->next);
+	if (head->str)
+	free(head->str);
+	free(head);
+	}
 }
 
