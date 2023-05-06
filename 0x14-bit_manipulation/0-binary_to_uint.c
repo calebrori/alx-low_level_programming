5896041a4a773a464d9f4a3018e91491379c7494
calebrori <calebrori@gmail.com>
2023-05-05 17:05:18 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,24 @@
 
 unsigned int convert_binary_to_unsigned_int(const char *binary_string)
 {
-    unsigned int result = 0;
-    int index = 0;
+	unsigned int result = 0;
+	int index = 0;
 
-    if (binary_string == NULL)
-        return 0;
+	if (binary_string == NULL)
+	return 0;
 
-    while (binary_string[index] != '\0')
-    {
-        if (binary_string[index] != '0' && binary_string[index] != '1')
-            return 0;
+	while (binary_string[index] != '\0')
+	{
+	if (binary_string[index] != '0' && binary_string[index] != '1')
+	return 0;
 
-        result <<= 1;
+	result <<= 1;
 
-        if (binary_string[index] == '1')
-            result += 1;
+	if (binary_string[index] == '1')
+	result += 1;
 
-        index++;
-    }
+	index++;
+	}
 
     return result;
 }
