aaa21dcdb73f99af2ca119d01f42b026614806cc
calebrori <calebrori@gmail.com>
2023-05-05 17:46:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
-	int index = 0;
+    unsigned int result = 0;
+    int index = 0;
 
-	if (b == NULL)
-	return (0);
+    if (b == NULL)
+        return (0);
 
-	while (b[index] != '\0')
-	{
-		if (b[index] != '0' && b[index] != '1')
-			return (0);
+    while (b[index] != '\0')
+    {
+        if (b[index] != '0' && b[index] != '1')
+            return (0);
 
-		result <<= 1;
+        result <<= 1;
 
-		if (b[index] == '1')
-		result += 1;
+        if (b[index] == '1')
+            result += 1;
 
-		index++;
-	}
+        index++;
+    }
 
-	return (result);
+    return (result);
 }
