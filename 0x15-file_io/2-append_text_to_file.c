dc618bb3f8058aba86e013cafdb47005a53716e8
calebrori <calebrori@gmail.com>
2023-05-09 16:29:11 +0300

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*append_text_to_file - function to append text at the end of a file
-*@filename: file to append
-*@text_content: string to add to file
-*Return: 1 on success and -1 on failure
+* append_text_to_file- function to append text at the end of a file
+* @filename: file to append
+* @text_content: string to add to file
+* Return: 1 on success and -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
