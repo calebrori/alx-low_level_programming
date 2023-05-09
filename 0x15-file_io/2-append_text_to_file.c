87a37c3b30e97bc0e1b9c2eadb34ce5af708a879
calebrori <calebrori@gmail.com>
2023-05-09 17:00:36 +0300

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,34 +8,35 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        ssize_t text_len, append_status;
-        int file_desc;
+	ssize_t text_len, append_status;
+	int file_desc;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        file_desc = open(filename, O_WRONLY | O_APPEND);
+	file_desc = open(filename, O_WRONLY | O_APPEND);
 
-        if (file_desc == -1)
-                return (-1);
+	if (file_desc == -1)
+		return (-1);
 
-        if (text_content == NULL)
-        {
-                close(file_desc);
-                return (1);
-        }
+	if (text_content == NULL)
+	{
+		close(file_desc);
+		return (1);
+	}
 
-        text_len = 0;
+	text_len = 0;
 
-        while (*(text_content + text_len))
-                text_len++;
+	while (*(text_content + text_len))
+		text_len++;
 
-        append_status = write(file_desc, text_content, text_len);
+	append_status = write(file_desc, text_content, text_len);
 
-        if (append_status == -1)
-        {
-                close(file_desc);
-                return (-1);
-        }       close(file_desc);
-        return (1);
+	if (append_status == -1)
+	{
+		close(file_desc);
+	return (-1);
+	}
+		close(file_desc);
+	return (1);
 }
