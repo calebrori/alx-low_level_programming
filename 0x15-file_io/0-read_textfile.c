a26e673fb2f843738864900c20fefc8e8f437f91
calebrori <calebrori@gmail.com>
2023-05-09 02:43:48 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,23 @@
 #include <stdlib.h>
 
 /*
- * read_textfile - function that reads a text file and prints it to
- *                 the POSIX standard output
- * @filename: name of the file to be read
- * @letters: number of letters it should read and print
- * Return: number of letters it could read and print
+ *read_textfile - function that reads a text file and prints it
+ *@filename: name of the file to be read
+ *@letters: number of letters
+ *Return: 1 on success, -1 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t file_descriptor, length, check;
 	char *buffer;
 
-	/* check if filename is null */
 	if (filename == NULL)
 		return (0);
 
-	/* create buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/* open file */
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 	{
@@ -30,7 +26,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* read file and error check */
 	length = read(file_descriptor, buffer, letters);
 	if (length == -1)
 	{
@@ -39,7 +34,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* print file to POSIX standard output */
 	check = write(STDOUT_FILENO, buffer, length);
 	if (check == -1 || length != check)
 	{
@@ -48,7 +42,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* close file, free buffer, and return length */
 	close(file_descriptor);
 	free(buffer);
 
