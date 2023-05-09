abc87fc4bda3506541d448ed249b0bced850cb90
calebrori <calebrori@gmail.com>
2023-05-09 02:53:35 +0000

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+/*
+ *create_file - function that creates a file and writes to it
+ *@filename: name of the file to be created
+ *@text_content: pointer to a string to write to the file
+ *
+ *Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+        int fd, res, len = 0;
+
+        if (filename == NULL)
+                return (-1);
+
+        fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+        if (fd == -1)
+                return (-1);
+
+        if (text_content != NULL)
+        {
+                	while (text_content[len] != '\0')
+                        len++;
+
+		res = write(fd, text_content, len);
+                if (res == -1 || res != len)
+                {
+                        close(fd);
+                        return (-1);
+                }
+        }
+	
+	close(fd);
+        return (1);
+}
