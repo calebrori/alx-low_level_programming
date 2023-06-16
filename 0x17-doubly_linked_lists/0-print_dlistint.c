004c07ff20e54588aadd423eb6a35b1bf75d2cc2
calebrori <calebrori@gmail.com>
2023-06-16 07:53:46 +0000

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
+#include "lists.h"
+
 /**
  * print_dlistint - function that prints all the elements of a list
  * @h: pointer to node
  *
  * Return: the number of nodes
  */
-
-#include "lists.h"
 
 size_t print_dlistint(const dlistint_t *h)
 
