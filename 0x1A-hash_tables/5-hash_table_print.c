95e7b79f779530585d79a6fa103e3bc3d2db43fd
caleb.rori <calebrori@gmail.com>
2023-07-08 02:48:09 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_print - Prints hash table
- * @ht: A pointer 
+ * @ht: A pointer
  *
  * Description: Key/value pairs are printed in the order
  *              they appear in the array of the hash table
