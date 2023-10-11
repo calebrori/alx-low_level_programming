15f238611e30a9521751219ba62a72ab7a5b1b17
calebrori <32507245+calebrori@users.noreply.github.com>
2023-10-11 23:27:25 +0300

    102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -9,33 +9,43 @@
   *
   * Return: -1, if the value is not present or the array is NULL
   */
-
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t c, d, e;
+	size_t pos, low, high;
+	double c;
 
-	/* Check if the array is NULL */
 	if (array == NULL)
 		return (-1);
 
-	/* Perform interpolation search */
-	for (d = 0, e = size - 1; e >= d;)
+	low = 0;
+	high = size - 1;
+
+	while (size)
 	{
-		c = d + (((double)(e - d) / (array[e] - array[d])) * (value - array[d]));
-		if (c < size)
-			printf("Value checked array[%ld] = [%d]\n", c, array[c]);
-		else
+		c = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + c);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
 		{
-			printf("Value checked array[%ld] is out of range\n", c);
+			printf(" is out of range\n");
 			break;
 		}
-
-		if (array[c] == value)
-			return (c);
-		if (array[c] > value)
-			e = c - 1;
 		else
-			l = c + 1;
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
 
 	return (-1);
